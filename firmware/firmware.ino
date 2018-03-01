bbe811a290f0ea5b62a7a5cdd444903cfc1072ef
5shekel <yair99@gmail.com>
2018-03-01 16:11:15 +0100

    calibrated to servo driver KC5188

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -13,21 +13,28 @@
   ATTinyCore- https://github.com/SpenceKonde/ATTinyCore
   sofServo http://playground.arduino.cc/ComponentLib/Servo
   SendOnlySoftwareSerial https://forum.arduino.cc/index.php?topic=112013.msg841582#msg841582
+  tiny_ir https://gist.github.com/SeeJayDee/caa9b5cc29246df44e45b8e7d1b1cdc5
 
+  attiny85 pinout - https://camo.githubusercontent.com/081b569122da2244ff7de8bae15eb56947d05cc8/687474703a2f2f6472617a7a792e636f6d2f652f696d672f50696e6f7574543835612e6a7067
  *******************************************************************/
-#include "tiny_IRremote.h"
+
 
 #define DEBUG 1
- 
+
 #define SERVO1PIN 0   // Servo control line (orange) on 0/PWM
-#define POTPIN   A2  // Potentiometer on 2/A1 //for some reason this pysically mapsto 4/A2/PB$ on my attiny85 *shrug*
+#define POTPIN   A3  // Potentiometer on A3
 #define TXPIN 1
+#define RECVPIN  4 //ir reciver data pin 0isfortimer
 
 #include "SoftwareServo.h"
 SoftwareServo myServo1;  // create servo object to control a servo
 
 #include "SendOnlySoftwareSerial.h"
 SendOnlySoftwareSerial mySerial (TXPIN);  // Tx pin
+
+#include "tiny_IRremote.h"
+IRrecv irrecv(RECVPIN);
+decode_results results;
 
 void setup() {
 
@@ -40,7 +47,16 @@ void setup() {
   delay(15);
 }
 
+
+
 void loop()  {
+
+
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+
   int servoPos;
   int potValue = analogRead(POTPIN);              // Read voltage on potentiometer
 
@@ -49,14 +65,15 @@ void loop()  {
   // in both directions, with a stop space in middle
 
   if ( potValue < 350 ) {
-    servoPos = map(potValue, 0, 350, 43, 83);
+    servoPos = map(potValue, 0, 350, 20, 83); //the88p0 has less 
   } else if (potValue > 650) {
-    servoPos = map(potValue, 1024, 650, 130, 89);
+    servoPos = map(potValue, 1024, 650, 140, 89);
   } else {
-    servoPos = 86;
+    servoPos = 82; //86 with the 880
   }
+  //to calibrate override the above
   //servoPos = map(potValue, 0, 1024, 0, 179);
-  
+
   mySerial.print(servoPos);
   mySerial.print(" ");
   mySerial.println(potValue);
