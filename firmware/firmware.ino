f6ec9f566825ea501f866a96a27cbcc46842a076
5shekel <yair99@gmail.com>
2018-03-04 16:17:32 +0100

    docs

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,13 +1,13 @@
 /*******************************************************************
 
    words http://idiot.io
-   code https://github.com/shenkarSElab/xxx
+   code https://github.com/shenkarSElab/tongue
 
    pin out attiny85
-   NC      -1+----+8-   VCC
-   3/A3    -2|*   |7-   2/A1
-   4/A4    -3|    |6-   1
-   GND     -4+----+5-   0/PWM
+   RST    -1+----+8-   VCC
+   3/LDR  -2|*   |7-   2/PWM
+   4/IR   -3|    |6-   1/SERIAL
+   GND    -4+----+5-   0/SERVO
 
   using:
   ATTinyCore- https://github.com/SpenceKonde/ATTinyCore
@@ -17,19 +17,47 @@
   ir_methods https://github.com/z3t0/Arduino-IRremote/wiki/Receiving-with-the-IRremote-library
 
   attiny85 pinout - https://camo.githubusercontent.com/081b569122da2244ff7de8bae15eb56947d05cc8/687474703a2f2f6472617a7a792e636f6d2f652f696d672f50696e6f7574543835612e6a7067
+ 
+ 
+ 
+  ==calibration==
+  the system has an intial guess for calibration, 
+  but you will need to fine tune it to compensate 
+  for diffrent material and lighting
+
+  [CH+] means press that key on remote
+  
+  *fine tune
+  cover LDR #1       press the [CH+] to set maxval
+  cover LDR #2       press the [CH-] to set minval
+  uncover both LDRs  press the [CH] to set midval
+
+  *trim
+  press [-] and [+] to subtract/add to trim value
+    after fine-tune you will have to trim the motor middle point
+    press until the motor doesnt turn or twitch in uncovered state.
+    ussualy no more then -/+10 is needed. 
+
+  *store
+  to put your settings in deep memory press the [200+] key
+    the setting you provide will be stored in ATtoni 
+    so they are remmbered and will  reboot.
+
+  *info
+  if DEBUG is on you can use the [EQ] button to watch your settings
+  you will need a serial pin connected to pin6 
  *******************************************************************/
 
-
-#define DEBUG 1
+#include <EEPROM.h>
 
 #define SERVO1PIN 0   // Servo control line (orange) on 0/PWM
-#define POTPIN   A3  // Potentiometer on A3
-#define TXPIN 1 // tested on PB1/PB4
-#define RECVPIN  4 //ir reciver data pin , tested on PB1/PB4
+#define TXPIN     1 // tested on PB1/PB4
+#define LEDPIN    2 // led pin
+#define POTPIN    3  // Potentiometer on A3
+#define RECVPIN   4 //ir reciver data pin , tested on PB1/PB4
 
 #include "SoftwareServo.h"
 SoftwareServo softServo;  // create servo object to control a servo
-int prevServoPos;
 
 #include "SendOnlySoftwareSerial.h"
 SendOnlySoftwareSerial softSerial (TXPIN);  // Tx pin
@@ -37,73 +65,188 @@ SendOnlySoftwareSerial softSerial (TXPIN);  // Tx pin
 #include "tiny_IRremote.h"
 IRrecv irrecv(RECVPIN);
 decode_results results;
-unsigned long  prevValue;
-long lastPressTime = 0;
 
-int pos_middle = 82;
+unsigned long  prevValue;
+
+struct settings {
+  int minval;
+  int midval;
+  int maxval; 
+  int trim;
+};
+settings set01 = {50,200,400,0 };
+
+int servoPos;
+int potValue, tmpval;
+  
+float x_1, y;
+int graph_state = 0;
 
 void setup() {
+
+  pinMode(LEDPIN, OUTPUT);
+  digitalWrite(LEDPIN, LOW);
+
   irrecv.enableIRIn();
 
   softSerial.begin(9600);
-  softSerial.println("hell");
+  softSerial.println("reboot");
+  
+  EEPROM.get(0, set01);
+  printdebug();
+  
 
   softServo.attach(SERVO1PIN);
-  softServo.setMaximumPulse(2200);
+  softServo.setMaximumPulse(2000);
 
   delay(15);
 }
 
 void loop()  {
+  //read IR signals
   if (irrecv.decode(&results)) {
     switch (results.value) {
       case 0xFFA25D:
         softSerial.println("CH-");
-        pos_middle--;
+	      set01.minval = servoPos;
+        printdebug();
         break;
+
+      case 0xFF629D: 
+        softSerial.println("CH");
+	      set01.midval = servoPos;
+        printdebug();
+        break;
+        
       case 0xFFE21D:
         softSerial.println("CH+");
-        pos_middle++;
+        set01.maxval = servoPos;
+        printdebug();
         break;
-      case 0xFF629D:
-        softSerial.println("CH");
+
+      case 0xFFE01F:
+        softSerial.println("-");
+        set01.trim++;
+        printdebug();
         break;
+
+      case 0xFFA857:
+        softSerial.println("+");
+        set01.trim--;
+        printdebug();
+        break;
+
+      case 0xFF52AD:
+        softSerial.println("9");
+        if (graph_state)
+          graph_state=0;
+        else 
+          graph_state=1;    
+          
+        break;
+
+      case 0xFF6897:
+        softSerial.println();
+        softSerial.println("0");
+        set01 = {50,200,500,0 };
+        printdebug();
+        break;
+
+      case 0xFF906F:
+        softSerial.println();
+        softSerial.println("EQ");
+        printdebug();
+        break;
+
+      case 0xFFB04F:
+        softSerial.println();
+        softSerial.println("200");
+        EEPROM.put(0, set01);
+        printdebug();
+        break;
+
       case 0xFFFFFFFF:
         break;
+
       default:
-        softSerial.print(F("unknown = 0x"));
+        softSerial.print(F("0x"));
         softSerial.println(results.value, HEX);
     }
-    irrecv.resume(); // Receive the next value
+            
 
+    irrecv.resume();
   }
 
-  int servoPos;
-  int potValue = analogRead(POTPIN);              // Read voltage on potentiometer
+  // Read voltage on LDR
+  potValue = analogRead(POTPIN);  
+ 
+  //smoothing
+  y = 0.10*potValue +0.75*y;
+  servoPos = int(y)  ;
+  tmpval = int(mapped(servoPos));
+  tmpval = constrain(tmpval, 0, 179);
 
-  // map the range of pot to limited map range on the servo.
-  // a narrow band of the full range.
-  // in both directions, with a stop space in middle
+  if (millis() - prevValue >= 15) {
+    prevValue = millis();
+    softServo.write(tmpval);
 
-  if ( potValue < 350 ) {
-    servoPos = map(potValue, 0, 350, 20, 80); //the88p0 has less
-  } else if (potValue > 650) {
-    servoPos = map(potValue, 1024, 650, 150, 90);
-  } else {
-    servoPos = pos_middle ;// 82; //86 with the 880
+    SoftwareServo::refresh();
+    
+	  //printdebug();
+    //printGraph();
   }
 
+}
 
-  //to calibrate override the above
-  //servoPos = map(potValue, 0, 1024, 0, 179);
-  if (servoPos != prevServoPos) {
-    softSerial.print(servoPos);
-    softSerial.print(" ");
-    softSerial.println(potValue);
-    softServo.write(servoPos);
+
+int mapped(int input){
+	int output;
+  if ( (input >= (set01.midval - 20) ) && (input <= (set01.midval + 20)) ){
+		output = 90-set01.trim ;
+    digitalWrite(LEDPIN, HIGH);
+	  } else if (input < (set01.midval - 20)){
+		output = map(input, set01.minval, set01.midval, 0, 90-set01.trim   );
+    digitalWrite(LEDPIN, LOW);
+    }else if(input > (set01.midval + 20)){
+		output = map(input, set01.midval, set01.maxval, 90-set01.trim  , 179 );
+    digitalWrite(LEDPIN, LOW);	
   }
-  prevServoPos = servoPos;
+	return output;
+}
 
-  SoftwareServo::refresh();
-  delay(15);                              // waits 15ms for the servo to reach the position
+void printGraph(){};
+
+  /*
+void printGraph(){
+  if(graph_state){
+    softSerial.print(tmpval);
+	  softSerial.print("	");
+	  softSerial.print(servoPos);
+	  softSerial.print("	");
+	  softSerial.print(potValue);
+    softSerial.println();
+  }
+}
+
+*/
+
+//void printdebug(){};
+void printdebug(){
+	//softSerial.println("minval  midval maxval  trim servopos");
+	softSerial.print(set01.minval);
+	softSerial.print("  ");
+	softSerial.print(set01.midval);
+	softSerial.print("  ");
+	softSerial.print(set01.maxval);
+  softSerial.print("  ");
+	softSerial.print(set01.trim);
+  softSerial.print("  ");
+	softSerial.print(servoPos);
+	//softSerial.println("tmpval	servoPos	potValue");
+	//softSerial.print(tmpval);
+	//softSerial.print("	");
+	//softSerial.print("	");
+	//softSerial.print(potValue);
+  softSerial.println();
+
 }
